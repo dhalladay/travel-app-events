3bbfccc216c2374619c435ca351cdd4c00d8c344
Jocelyn Tian <jocelyntianww@gmail.com>
2021-12-11 10:39:20 -0700

    made opentripmap API work

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var futureCityInput = document.querySelector("#future-city");
 var futureDateInput = document.querySelector("#future-date");
 
 //need to add functionality to return to homepage if search fails or strings empty
-$('#submit-trip').click(function(event) {
+$("#submit-trip").click(function (event) {
   event.preventDefault();
   if (!cityNameInput.value || !startDateInput.value || !endDateInput.value) {
     $("#missing-param-modal").modal("show");
@@ -15,65 +15,87 @@ $('#submit-trip').click(function(event) {
     $("#date-order-modal").modal("show");
   } else {
     var citySearch = cityNameInput.value
-    .trim()
-    .toLowerCase()
-    .replaceAll(" ", "+");
+      .trim()
+      .toLowerCase()
+      .replaceAll(" ", "+");
     var startSearch = startDateInput.value;
     //add a day to end date as ticketmaster api uses events BEFORE end date query parameter
-    var endSearch = moment(endDateInput.value).add(2, 'days').format("YYYY-MM-DD");
-    window.location.href = "./HTML/events.html?city=" + citySearch + "&startDate=" + startSearch + "&endDate=" + endSearch;  
+    var endSearch = moment(endDateInput.value)
+      .add(2, "days")
+      .format("YYYY-MM-DD");
+    window.location.href =
+      "./HTML/events.html?city=" +
+      citySearch +
+      "&startDate=" +
+      startSearch +
+      "&endDate=" +
+      endSearch;
+  }
 
-//search today's events eventlistener
-$('#find-today-events').click(function(event) {
-  event.preventDefault();
-  if (!currentCityInput.value) {
-    $("#missing-param-modal").modal("show");
-  } else {
-    //city search formatting
-    var citySearch = currentCityInput.value
-    .trim()
-    .toLowerCase()
-    .replaceAll(" ", "+");
-    //get todays date and format
-    var startSearch = moment(new Date()).format("YYYY-MM-DD");
-    //get next days date and format
-    var endSearch = moment(startSearch).add(2, 'days').format("YYYY-MM-DD");
-    //send to events page
-    // console.log(citySearch, startSearch, endSearch);
-    window.location.href = "./HTML/events.html?city=" + citySearch + "&startDate=" + startSearch + "&endDate=" + endSearch;  
-  };
+  //search today's events eventlistener
+  $("#find-today-events").click(function (event) {
+    event.preventDefault();
+    if (!currentCityInput.value) {
+      $("#missing-param-modal").modal("show");
+    } else {
+      //city search formatting
+      var citySearch = currentCityInput.value
+        .trim()
+        .toLowerCase()
+        .replaceAll(" ", "+");
+      //get todays date and format
+      var startSearch = moment(new Date()).format("YYYY-MM-DD");
+      //get next days date and format
+      var endSearch = moment(startSearch).add(2, "days").format("YYYY-MM-DD");
+      //send to events page
+      // console.log(citySearch, startSearch, endSearch);
+      window.location.href =
+        "./HTML/events.html?city=" +
+        citySearch +
+        "&startDate=" +
+        startSearch +
+        "&endDate=" +
+        endSearch;
+    }
+  });
+
+  //search for future events on a single day eventlistener
+  $("#find-future-events").click(function (event) {
+    event.preventDefault();
+    if (!futureCityInput.value || !futureDateInput.value) {
+      $("#missing-param-modal").modal("show");
+    } else {
+      //get city and format
+      var citySearch = futureCityInput.value
+        .trim()
+        .toLowerCase()
+        .replaceAll(" ", "+");
+      //get future date
+      var startSearch = futureDateInput.value;
+      //get next days date for search
+      var endSearch = moment(futureDateInput.value)
+        .add(2, "days")
+        .format("YYYY-MM-DD");
+      // console.log(citySearch, startSearch, endSearch);
+      window.location.href =
+        "./HTML/events.html?city=" +
+        citySearch +
+        "&startDate=" +
+        startSearch +
+        "&endDate=" +
+        endSearch;
+    }
+  });
 });
 
-//search for future events on a single day eventlistener
-$('#find-future-events').click(function(event) {
-  event.preventDefault();
-  if (!futureCityInput.value || !futureDateInput.value) {
-    $("#missing-param-modal").modal("show");
-  } else {
-    //get city and format
-    var citySearch = futureCityInput.value
-    .trim()
-    .toLowerCase()
-    .replaceAll(" ", "+");
-    //get future date
-    var startSearch = futureDateInput.value;
-    //get next days date for search
-    var endSearch = moment(futureDateInput.value).add(2, 'days').format('YYYY-MM-DD');
-    // console.log(citySearch, startSearch, endSearch);
-    window.location.href = "./HTML/events.html?city=" + citySearch + "&startDate=" + startSearch + "&endDate=" + endSearch;  
-  };
-});
- 
-//plan a trip 
+//plan a trip
 //opens a modal
 //user enters city, start and end date
 //event listener on modal submit button
 //gather city name, start date, end date, county code and create an array
 //plug the array data into a api fetch requests (holiday and ticketmaster)
 
-
 //pass info from ticket master and holiday functions to create the events display page
-
 
 //today
 //open modal
@@ -83,13 +105,12 @@ $('#find-future-events').click(function(event) {
 //plug the array data into a api functions (holiday and ticketmaster )
 //pass info from ticket master and holiday functions to create the events display page
 
-
 //display page
 //dynamically create notification card for the holiday
-  //says it is <holiday> in <city>
+//says it is <holiday> in <city>
 //dynamically created ticket master events listed
-  //show the name, date, price
-//user can select events they are interest in 
+//show the name, date, price
+//user can select events they are interest in
 //ability to add their own events, free form text field and date
 //event listener on the display page to save to trips, takes them to my trips page
 
